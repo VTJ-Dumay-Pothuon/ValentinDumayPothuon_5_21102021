b7b8b3f363dbe00f793f6364748b8e650ff8a747
Valentin Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2021-11-01 20:16:58 +0100

    01112021-2 : Created a function adding listeners to form fields and check their content by RegEx

--- front/js/cart.js
+++ front/js/cart.js
@@ -45,6 +45,53 @@ function newArticle (item, order) {
 }
 
 
+// Regex patterns used for the form entries. In short:
+//– Name pattern allows for most latin character, hyphen, and space.
+//– Address pattern allows for the full name pattern, plus digits.
+//– Mail Pattern's username allows for address pattern -space  +underscore
+//––– Full mail pattern MUST be addressPattern@lowercase-or-digits.lowercase
+const namePattern = new RegExp ( "^[A-Za-zÀ-ÖØ-öø-ſ\\-\\ ]+$" )    ;
+const addressPattern = new RegExp("^[0-9"+namePattern.source.slice(2));
+const emailPattern = new RegExp( addressPattern.source.substring (   //
+  0, addressPattern.source.length - 5)+"\\.\\_]+@[0-9a-z\\-]+\\.[a-z]+$");
+
+// Default error message for Form invalid entries. 'false' means erase.
+function errorMessage (field, display=true,
+// If you want to customize displayMessage, you must add 'true' before.
+displayMessage="Un ou plus plusieurs caractères invalides !") {
+  if (display) {
+    document.getElementById(field+"ErrorMsg").innerText = displayMessage;
+  } else {
+    document.getElementById(field+"ErrorMsg").innerText = "";
+  }
+}
+
+
+// Add an event listener to each form field with specific RegEx patterns to check on
+function setFields () {
+  let fields = [["firstName",namePattern],
+                 ["lastName",namePattern],
+                  ["address",addressPattern],
+                     ["city",namePattern],
+                    ["email",emailPattern]]
+            
+  for (let field of fields) {
+    document.getElementById(field[0]).addEventListener("input", function(e) {
+      if (field[1].test(e.target.value)) {
+        errorMessage(field[0],false);
+      } else {
+        if (field[0]==="email") {
+          errorMessage(field[0],true,"Ceci n'est pas une adresse email valide !");
+        } else {
+          errorMessage(field[0]);
+        }
+      }
+    });
+  }
+}
+
+
+
 fetch('http://localhost:3000/api/products')
 
   .then(function (response) {
@@ -57,64 +104,65 @@ fetch('http://localhost:3000/api/products')
 
 
   .then(function (data) {
-    let cart = JSON.parse(localStorage.getItem('cart'));  
+    let cart = JSON.parse(localStorage.getItem('cart'));
     for (let inCart of cart) {
-        console.log(cart);
-        
-        for (let item of data) {
-          // console.log(item);
+      console.log(cart);
+      
+      for (let item of data) {
+        // console.log(item);
 
-          // Check if inCart article is valid and is the currently parsed item
-          // Here the color doesn't matter. Just checking for API data to show
-          if (inCart.id===item._id) {
-            console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
-            
-            newArticle (item, inCart) ;
-            updateTotal(inCart, item) ;
+        // Check if inCart article is valid and is the currently parsed item
+        // Here the color doesn't matter. Just checking for API data to show
+        if (inCart.id===item._id) {
+          console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
+          
+          newArticle (item, inCart) ;
+          updateTotal(inCart, item) ;
 
-            // Select the article block based on a pair of data parameters,
-            // Then, find the quantity dropdown in the standardized structure
-            let article = document.querySelectorAll(
-            '[data-id="'+inCart.id+'"][data-color="'+inCart.color+'"]')[0];
+          // Select the article block based on a pair of data parameters,
+          // Then, find the quantity dropdown in the standardized structure
+          let article = document.querySelectorAll(
+          '[data-id="'+inCart.id+'"][data-color="'+inCart.color+'"]')[0];
+          // console.log(article);
+          let checkQuantity = article.children[1].children[1].children[0].children[1];
+          // console.log(checkQuantity.value);
+
+          // For each individual article-color pair, an event listener is put onto the quantity
+          // On change, it updates the quantity in local storage, and reloads the total values
+          checkQuantity.addEventListener('change', function() {
             // console.log(article);
-            let checkQuantity = article.children[1].children[1].children[0].children[1];
-            // console.log(checkQuantity.value);
+            // First, temporarily remove the updated article from total
+            removeFromTotal(inCart, item);
 
-            // For each individual article-color pair, an event listener is put onto the quantity
-            // On change, it updates the quantity in local storage, and reloads the total values
-            checkQuantity.addEventListener('change', function() {
-              // console.log(article);
-              // First, temporarily remove the updated article from total
-              removeFromTotal(inCart, item);
+            // Second, update the quantity set in local storage
+            inCart.quantity = checkQuantity.value;
+            localStorage.setItem('cart', JSON.stringify(cart));
+            // console.log(cart);
+            // console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
 
-              // Second, update the quantity set in local storage
-              inCart.quantity = checkQuantity.value;
-              localStorage.setItem('cart', JSON.stringify(cart));
-              // console.log(cart);
-              // console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
-
-              // Third, re-add it to total, with the new quantity
-              updateTotal(inCart, item);
-            });
+            // Third, re-add it to total, with the new quantity
+            updateTotal(inCart, item);
+          });
 
 
-            // Add an event listener to the deleted button so that articles can be deleted
-            let deleteArticle = article.children[1].children[1].children[1].children[0];
-            deleteArticle.addEventListener('click', function() {
-              removeFromTotal(inCart, item);
-              inCart.quantity = "0";
-              cart.splice(cart.indexOf(inCart), 1);
-              localStorage.setItem('cart', JSON.stringify(cart));
-              article.remove();
-              updateTotal(inCart, item);
-            });
+          // Add an event listener to the deleted button so that articles can be deleted
+          let deleteArticle = article.children[1].children[1].children[1].children[0];
+          deleteArticle.addEventListener('click', function() {
+            removeFromTotal(inCart, item);
+            inCart.quantity = "0";
+            cart.splice(cart.indexOf(inCart), 1);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            article.remove();
+            updateTotal(inCart, item);
+          });
 
 
-            // Stop searching the article in the catalogue, as it's already found
-            break;
-          }
+          // Stop searching the article in the catalogue, as it's already found
+          break;
         }
+      }
     }
+  setFields();
 })
 .catch(function (error) {
   console.log(error);
