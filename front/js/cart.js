3dbfa8da688d919eae5b3e187e213c683d68c3ed
Valentin Dumay-Pothuon <valentin.dumay.pothuon@gmail.com>
2021-11-02 20:31:42 +0100

    02112021-1 : Order POST request validated and orderID sent to confirmation.html

--- front/js/cart.js
+++ front/js/cart.js
@@ -46,8 +46,8 @@ function newArticle (item, order) {
 
 
 // Regex patterns used for the form entries. In short:
-//– Name pattern allows for most latin character, hyphen, and space.
-//– Address pattern allows for the full name pattern, plus digits.
+//– Name pattern allows for most latin character, hyphen, and space
+//– Address pattern allows for the full name pattern, plus digits
 //– Mail Pattern's username allows for address pattern -space  +underscore
 //––– Full mail pattern MUST be addressPattern@lowercase-or-digits.lowercase
 const namePattern = new RegExp ( "^[A-Za-zÀ-ÖØ-öø-ſ\\-\\ ]+$" )    ;
@@ -66,30 +66,109 @@ displayMessage="Un ou plus plusieurs caractères invalides !") {
   }
 }
 
+// Pseudo-object that associates a form field, a RegEx pattern, and a validation boolean
+let fields = [["firstName",namePattern,false],
+               ["lastName",namePattern,false],
+                ["address",addressPattern,false],
+                   ["city",namePattern,false],
+                  ["email",emailPattern,false]];
 
 // Add an event listener to each form field with specific RegEx patterns to check on
 function setFields () {
-  let fields = [["firstName",namePattern],
-                 ["lastName",namePattern],
-                  ["address",addressPattern],
-                     ["city",namePattern],
-                    ["email",emailPattern]]
-            
   for (let field of fields) {
     document.getElementById(field[0]).addEventListener("input", function(e) {
       if (field[1].test(e.target.value)) {
         errorMessage(field[0],false);
+        field[2] = true;
+        // console.log (field[0]+" : "+field[2]);
       } else {
         if (field[0]==="email") {
           errorMessage(field[0],true,"Ceci n'est pas une adresse email valide !");
         } else {
           errorMessage(field[0]);
         }
+        field[2] = false;
       }
     });
   }
+  // Return true only if ALL fields are valid
+  return fields[0][2] && fields[1][2] && fields[2][2] && fields[3][2] && fields[4][2];
 }
 
+// Check if all fields are valid and the cart isn't empty
+function setOrderButton(cart) {
+  let validForm = false;
+  document.getElementById('order').addEventListener('click', function(e) {
+    e.preventDefault();
+    validForm = setFields() && cart.length>0;
+    if (validForm) {
+      sendOrder(cart);
+    } else {
+      if (!cart || cart.length<1) {
+        alert("Le panier est vide !");
+      } else {
+        alert("Le formulaire est invalide !");
+      }
+    }
+  });
+}
+
+
+// Build up a contact object containing the fields' informations
+function setContact () {
+  let contact={firstName:"",lastName:"",address:"",city:"",email:""};
+  contact.firstName = document.getElementById('firstName').value;
+   contact.lastName = document.getElementById('lastName').value;
+    contact.address = document.getElementById('address').value;
+       contact.city = document.getElementById('city').value;
+      contact.email = document.getElementById('email').value;
+  return contact;
+}
+
+
+// Build up an array of product-IDs from what's in the cart
+function setProducts (cart) {
+  let products = [];
+  for (let inCart of cart) {
+    products.push(inCart.id);
+  }
+  return products;
+}
+
+
+function sendOrder(cart) {
+  const contact = setContact();
+  const products = setProducts(cart);
+  fetch("http://localhost:3000/api/products/order", {
+     
+    // Adding method type
+    method: "POST",
+     
+    // Adding body or contents to send
+    body: JSON.stringify({
+        contact: contact,
+        products: products
+    }),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  
+  .then(function (response) {
+    //console.log(response.json());
+    return response.json();
+  })
+  .catch(function (error) {
+      console.log(error);
+    })
+
+  .then(function (order) {
+    // console.log(order.orderId);
+    window.location.href = './confirmation.html?orderId='+order.orderId;
+  })
+} 
 
 
 fetch('http://localhost:3000/api/products')
@@ -105,16 +184,15 @@ fetch('http://localhost:3000/api/products')
 
   .then(function (data) {
     let cart = JSON.parse(localStorage.getItem('cart'));
+    // console.log(cart);
     for (let inCart of cart) {
-      console.log(cart);
-      
       for (let item of data) {
         // console.log(item);
 
         // Check if inCart article is valid and is the currently parsed item
         // Here the color doesn't matter. Just checking for API data to show
         if (inCart.id===item._id) {
-          console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
+          // console.log("— "+inCart.quantity+" "+item.name+" "+inCart.color);
           
           newArticle (item, inCart) ;
           updateTotal(inCart, item) ;
@@ -162,8 +240,9 @@ fetch('http://localhost:3000/api/products')
         }
       }
     }
-  setFields();
-})
-.catch(function (error) {
-  console.log(error);
-});
+    setFields();
+    setOrderButton(cart);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
